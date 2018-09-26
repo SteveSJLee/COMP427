717d5b5f4582e7607936e326f5cc3bb7be7d6cd2
stevesjlee <seung.jin.lee@mail.mcgill.ca>
2018-09-25 22:26:09 -0400

    update

--- A1/tiny_shell.c
+++ A1/tiny_shell.c
@@ -1,1 +1,28 @@
 // tiny shell
+#include <stdlib.h>
+#include <stdio.h>
+
+//  tiny shell with built in system function
+void system_shell() 
+{
+	char *line = NULL;
+	size_t n = 0;
+	ssize_t len = 0;
+	while (1) {
+		len = getline(&line, &n, stdin);
+		if (len > 1)
+			system(line);
+		else
+			exit(0);
+	}
+}
+
+
+
+int main(int argc, char *argv[]) 
+{
+	printf("Tiny shell implementation using Linux system")
+	system_shell();
+
+	return 0;
+}
