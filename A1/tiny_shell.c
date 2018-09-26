63e8fb56d01fc050a9f6b988be455cd6f7cae59c
stevesjlee <seung.jin.lee@mail.mcgill.ca>
2018-09-25 22:53:17 -0400

    update

--- A1/tiny_shell.c
+++ A1/tiny_shell.c
@@ -2,8 +2,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/* funtion prototypes*/
+void tshell(void);
+int my_system(char* line);
+int clone_function(void* arg);
+
+
+
+
+
+int my_system(char* line)
+{
+	#ifdef FORK
+	// version F
+
+	#elif VFORK
+	// version V
+
+	#elif CLONE
+	// version C
+
+	#elif PIPE
+	// version P
+
+	#else 
+		// default will use built in system fn
+		system(line);
+	#endif
+
+}
+
+
 //  tiny shell with built in system function
-void system_shell() 
+void tshell() 
 {
 	char *line = NULL;
 	size_t n = 0;
@@ -11,18 +42,18 @@ void system_shell()
 	while (1) {
 		len = getline(&line, &n, stdin);
 		if (len > 1)
-			system(line);
+			my_system(line);
 		else
 			exit(0);
 	}
 }
 
 
-
+// main
 int main(int argc, char *argv[]) 
 {
 	printf("Tiny shell implementation using Linux system")
-	system_shell();
+	tshell();
 
 	return 0;
 }
